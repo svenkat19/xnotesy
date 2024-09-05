637e1a82580b356f1a77765eb23b3d4304c2bf35
shashyv <shashyv@gmail.com>
2024-09-05 22:51:39 +0530

    Routers added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Ghar from "./components/Ghar";
 
 function App() {
   return (
-    <div className="App">
-     <h1>This is Xnotesy</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Ghar />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Ghar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Ghar() {
+  return (
+    <div>
+      this is home
+    </div>
+  )
+}
+
+export default Ghar
